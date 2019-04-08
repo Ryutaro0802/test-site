df2e4a613ec627acd11161193b11ae4308bdbd29
Ryutaro0802 <ne200152g@gmail.com>
2019-04-09 02:23:48 +0900

    unixTimeを変換

--- store/articles.js
+++ store/articles.js
@@ -1,5 +1,6 @@
 import firebase from '~/plugins/firebase';
 import { firebaseAction } from 'vuexfire';
+import dayjs from 'dayjs';
 
 const firestore = firebase.firestore();
 const articlesCollection = firestore.collection('articles');
@@ -14,6 +15,8 @@ export const getters = {
     const tags = rootState.tags.tags;
     return state.articles.map(article => {
       article.tags = tags.filter(tag => article.tagIds.includes(tag.id));
+      article.createdAt = dayjs(article.createdAt.nanoseconds).format('YYYY-MM-DD');
+      article.updatedAt = dayjs(article.updatedAt.nanoseconds).format('YYYY-MM-DD');
       return article;
     });
   },
